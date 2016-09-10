52130007e9f68b4106cbfda9b82e4acac36dd436
Anjali Sharma <sharma.anjali3110@gmail.com>
2016-09-10 17:35:36 +0530

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root :to => 'users#index'
-
+  get 'static_pages/home'
+  get 'static_pages/help'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
   resources :users do
     collection do
       get 'new_request' => 'users#new_request'
