d9c317db8a96a9d25b4081e3b09d7ea65dc00ef7
Jeet Banerjee <jeet.23nz@gmail.com>
2016-09-10 23:49:18 +0530

    Fixed index request display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,24 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-    # @requests = Request.where.not(user_id: current_user.id)
+    get_available_users
+    @requests = Request.where("user_id in (?) ",@list_of_users)
 	end
 
-  def new_request
-    current_user.update(current_sign_in_ip: request.remote_ip) if current_user.address.blank?
-    distance = 10
+  def get_available_users
+    distance = 1000
     center_point = [current_user.latitude, current_user.longitude]
     box = Geocoder::Calculations.bounding_box(center_point, distance)
     a = User.within_bounding_box(box)
-    @list_of_users = a.reject{|b| b.id == current_user.id}
-    #add_address if params["address"].present?
-    request_added = Request.add_new_request(params, current_user)
+    users = a.reject{|b| b.id == current_user.id}
+    @list_of_users = users.map(&:id)
+  end
+
+  def new_request
+    current_user.update(current_sign_in_ip: request.remote_ip) if current_user.address.blank?
+    
+    # add_address if params["address"].present?
+    # request_added = Request.add_new_request(params, current_user)
     redirect_to :action => 'index'
   end
 
