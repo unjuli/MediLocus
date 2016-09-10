eaa373788eefb0cfd0ef2ad1beeb012936dbe073
Jeet Banerjee <jeet.23nz@gmail.com>
2016-09-10 18:52:23 +0530

    merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
   devise_for :users
 
-  root :to => 'users#index'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # root :to => 'users#index'
   resources :users do
     collection do
       get 'new_request' => 'users#new_request'
